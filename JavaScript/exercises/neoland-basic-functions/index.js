ba529d4c2f666ec721549bfdfc3e9b9af0433d9b
Alex Tomas <atomasllimos@gmail.com>
2022-09-26 17:26:47 +0200

    Iteration#2

--- JavaScript/exercises/neoland-basic-functions/index.js
+++ JavaScript/exercises/neoland-basic-functions/index.js
@@ -1,4 +1,4 @@
-//Iteración #1: Buscar el máximo**
+// Iteración #1: Buscar el máximo**
 // Completa la función que tomando dos números como argumento devuelva el más alto.
 const sum = (numberOne, numberTwo) => {
     if (numberOne > numberTwo) console.log(numberOne)
@@ -6,3 +6,18 @@ const sum = (numberOne, numberTwo) => {
     else console.log('Both numbers are equal')
 }
 sum(10, 10)
+
+// Iteración #2: Buscar la palabra más larga**
+// Completa la función que tomando un array de strings como argumento devuelva el más largo, en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+const findLongestWord = (param) => {
+    if (!Array.isArray(param)) console.log('You must set an array as a parameter')
+    let wordsLength = []
+    for(word of param) {
+        wordsLength.push(word.length)
+    }
+    const longestWords = param.filter(w => w.length === Math.max(...wordsLength))
+    
+    console.log(longestWords[0])
+}
+findLongestWord(avengers)
