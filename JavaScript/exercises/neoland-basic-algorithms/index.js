f1c67d9f8c83ee25b1085ed8bfe4ee76fa9d58ea
Alex Tomas <atomasllimos@gmail.com>
2022-09-26 16:43:19 +0200

    Iteration#2

--- JavaScript/exercises/neoland-basic-algorithms/index.js
+++ JavaScript/exercises/neoland-basic-algorithms/index.js
@@ -8,3 +8,15 @@ let h = 5
 let y = 10
 // 1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
 let z = h + y
+
+// Iteración #2: Variables avanzadas
+const character = {name: 'Jack Sparrow', age: 10}
+// 1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
+character.age = 25
+
+// 1.2 Declara 3 variables con los nombres y valores siguientes 
+let firstName = 'Jon'
+let lastName = 'Snow'
+let age = 24
+
+console.log(`Soy ${firstName} ${lastName}, tengo ${age} y me gustan los lobos`)
