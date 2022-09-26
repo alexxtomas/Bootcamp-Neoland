bbef9b6d7bab549ddaf6d45d01b65ee1cd06b1a9
Alex Tomas <atomasllimos@gmail.com>
2022-09-26 17:38:08 +0200

    Iteration#3

--- JavaScript/exercises/neoland-basic-functions/index.js
+++ JavaScript/exercises/neoland-basic-functions/index.js
@@ -12,12 +12,28 @@ sum(10, 10)
 const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
 const findLongestWord = (param) => {
     if (!Array.isArray(param)) console.log('You must set an array as a parameter')
-    let wordsLength = []
-    for(word of param) {
-        wordsLength.push(word.length)
+    else {
+        let wordsLength = []
+        for(word of param) {
+            wordsLength.push(word.length)
+        }
+        const longestWords = param.filter(w => w.length === Math.max(...wordsLength))
+        
+        console.log(longestWords[0])
     }
-    const longestWords = param.filter(w => w.length === Math.max(...wordsLength))
-    
-    console.log(longestWords[0])
+  
 }
-findLongestWord(avengers)
+findLongestWord('a')
+
+//Iteración #3: Calcular la suma**
+
+/*Calcular una suma puede ser tan simple como iterar sobre un array y sumar cada uno de los elementos.
+Implemente la función denominada sumNumbers que toma un array de números como argumento y devuelve la suma de todos los números de la matriz.*/
+
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+
+const sumAll = param => {
+    if (!Array.isArray(param)) console.log('You must set an array as a parameter')
+    else console.log(param.reduce((prev, curr) => prev + curr, 0))
+}
+sumAll(numbers)
