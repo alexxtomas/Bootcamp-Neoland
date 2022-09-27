96129bca182b3527cc2a9ef815e142faa3457e4b
Alex Tomas <atomasllimos@gmail.com>
2022-09-27 14:39:29 +0200

    Iteration#5

--- JavaScript/exercises/neoland-loops/index.js
+++ JavaScript/exercises/neoland-loops/index.js
@@ -51,3 +51,15 @@ const alien = {
 for (let prop in alien) {
     console.log(alien[prop])
 }
+
+//Iteración #5: Probando For//
+
+// Usa un bucle for para recorrer todos los destinos del array y elimina los elementos que tengan el id 11 y 40. Imprime en un console log el array.
+const placesToTravelObj = [{id: 5, name: 'Japan'}, {id: 11, name: 'Venecia'}, {id: 23, name: 'Murcia'}, {id: 40, name: 'Santander'}, {id: 44, name: 'Filipinas'}, {id: 59, name: 'Madagascar'}]
+
+for(let i in placesToTravelObj) {
+   const {id} = placesToTravelObj[i]
+   if(id === 11 || id === 40) placesToTravelObj.splice(i, 1)
+}
+
+console.log(placesToTravelObj)
