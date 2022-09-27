af92603465350fdb00252988dfba55345ec8528b
Alex Tomas <atomasllimos@gmail.com>
2022-09-27 11:02:45 +0200

    Iteration#7

--- JavaScript/exercises/neoland-basic-functions/index.js
+++ JavaScript/exercises/neoland-basic-functions/index.js
@@ -116,3 +116,37 @@ function removeDuplicates(param) {
 const f = removeDuplicates(duplicates)
 console.log(f)
 
+// Iteración #7: Buscador de nombres
+
+// Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array - comprueba si existe el elemento, en caso que existan nos devuelve un true y la posición de dicho elemento y por la contra un false. Puedes usar este array para probar tu función:
+
+const nameFinder = [
+  'Peter',
+  'Steve',
+  'Tony',
+  'Natasha',
+  'Clint',
+  'Logan',
+  'Xabier',
+  'Bruce',
+  'Peggy',
+  'Jessica',
+  'Marc',
+  'Steve'
+];
+function finderName(array, nameToFind) {
+  let result = {
+  }
+  array.forEach(n => {
+    if(n === nameToFind) {
+      result.index = array.indexOf(n)
+    }
+  })
+
+  if (!result?.index) return false
+
+  return [result, true]
+}
+
+const j = finderName(nameFinder, 'Tony')
+console.log(j)
