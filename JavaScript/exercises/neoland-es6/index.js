9b934a24c3062981a22a3b4174b4c43e7f700937
Alex Tomas <atomasllimos@gmail.com>
2022-09-28 17:28:37 +0200

    Iteration#4

--- JavaScript/exercises/neoland-es6/index.js
+++ JavaScript/exercises/neoland-es6/index.js
@@ -88,3 +88,47 @@ const allToys = {...toy2, ...toyUpdate}
 pero sin editar el array inicial. De nuevo, usando spread operatos. */
 const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
 const colorsWithoutYellow = [colors[0], colors[1], ...colors.slice(3)]
+
+// Iteración #4: Map
+
+/* 4.1 Dado el siguiente array, devuelve un array con sus nombres 
+utilizando .map(). */
+const users = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
+
+const userNames = users.map(({name}) => name)
+
+/* 4.2 Dado el siguiente array, devuelve una lista que contenga los valores 
+de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que 
+empiece por 'A'. */
+const users2 = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
+
+const userNames2 = users2.map(({name}) => {
+    if (name.startsWith('A')) return 'Anacleto'
+    return name
+})
+
+/* 4.3 Dado el siguiente array, devuelve una lista que contenga los valores 
+de la propiedad .name y añade al valor de .name el string ' (Visitado)' 
+cuando el valor de la propiedad isVisited = true. */
+const cities = [
+	{isVisited:true, name: 'Tokyo'}, 
+	{isVisited:false, name: 'Madagascar'},
+	{isVisited:true, name: 'Amsterdam'}, 
+	{isVisited:false, name: 'Seul'}
+];
+
+const visitedCities = cities.map(({isVisited, name}) => {
+    if(isVisited) return `${name} (Visitado)`
+    return name
+})
+
