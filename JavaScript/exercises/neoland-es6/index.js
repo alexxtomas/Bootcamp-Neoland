5ddf186400453737318722546613ffeec9eb99bf
Alex Tomas <atomasllimos@gmail.com>
2022-09-28 17:16:45 +0200

    Iteration#3

--- JavaScript/exercises/neoland-es6/index.js
+++ JavaScript/exercises/neoland-es6/index.js
@@ -59,3 +59,32 @@ console.log(allItvs)
 console.log(itv1)
 console.log(itv2)
 console.log(itv3)
+
+// Iteración #3: Spread Operator
+// 3.1 Dado el siguiente array, crea una copia usando spread operators.
+const pointsList = [32, 54, 21, 64, 75, 43]
+const pointsListCopy = [...pointsList]
+
+
+// 3.2 Dado el siguiente objeto, crea una copia usando spread operators.
+const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const toyCopy = {...toy}
+
+/* 3.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando 
+spread operatos. */
+const pointsList2 = [32, 54, 21, 64, 75, 43];
+const pointsList3 = [54,87,99,65,32];
+
+const allPoints = [...pointsList2, ...pointsList3]
+
+/* 3.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos 
+con spread operators. */
+const toy2 = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']}
+
+const allToys = {...toy2, ...toyUpdate}
+
+/* 3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2 
+pero sin editar el array inicial. De nuevo, usando spread operatos. */
+const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
+const colorsWithoutYellow = [colors[0], colors[1], ...colors.slice(3)]
