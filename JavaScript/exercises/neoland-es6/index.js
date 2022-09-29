62202106f769feccbb55d34d351fa47e2e47ea9b
Alex Tomas <atomasllimos@gmail.com>
2022-09-29 12:27:16 +0200

    Iteration#7

--- JavaScript/exercises/neoland-es6/index.js
+++ JavaScript/exercises/neoland-es6/index.js
@@ -214,3 +214,37 @@ const selectedAlien = aliens.find(({name}) => name === 'Cucushumushu')
 const selectedMutation = mutations.find(({name}) => name === 'Porompompero')
 
 const alienAndMutation = {...selectedAlien, mutation: selectedMutation}
+
+
+/* 7.1 Dado el siguiente array, haz una suma de todos las notas de los examenes de 
+los alumnos usando la función .reduce(). */
+
+const exams = [
+    {name: 'Yuyu Cabeza Crack', score: 5}, 
+    {name: 'Maria Aranda Jimenez', score: 1}, 
+    {name: 'Cristóbal Martínez Lorenzo', score: 6}, 
+    {name: 'Mercedez Regrera Brito', score: 7},
+    {name: 'Pamela Anderson', score: 3},
+    {name: 'Enrique Perez Lijó', score: 6},
+    {name: 'Pedro Benitez Pacheco', score: 8},
+    {name: 'Ayumi Hamasaki', score: 4},
+    {name: 'Robert Kiyosaki', score: 2},
+    {name: 'Keanu Reeves', score: 10}
+]
+
+const allScores = exams.reduce((acc, {score}) => acc + score, 0)
+/* 
+7.2 Dado el mismo array, haz una suma de todos las notas de los examenes de los 
+alumnos que esten aprobados usando la función .reduce(). */
+
+const aprrovedExams = exams.reduce((acc, {score}) => {
+	if(score < 5)  score = 0
+	return acc + score
+}, 0)
+
+
+
+/* 7.3 Dado el mismo array, haz la media de las notas de todos los examenes .reduce(). */
+const average = exams.reduce((acc, {score}) => acc + score, 0)
+					 / exams.length
+	
